c15dcaf15e10f98ae6797f57c8d3a5dfbd1ecd85
Chidera Mosanya <camosanya@gmail.com>
2023-07-03 09:19:40 -0400

    twitter streaming code skeleton

--- app/engine/data/producers/tweet.py
+++
@@ -1,9 +0,0 @@
-import tweepy
-from confluent_kafka.avro import AvroProducer
-
-
-class TweetStreamClient(tweepy.StreamingClient):
-
-    def __init__(self, *args, **kwargs):
-        super(TweetStreamClient, self).__init__(*args, **kwargs)
-        self.kafka_producer = None
